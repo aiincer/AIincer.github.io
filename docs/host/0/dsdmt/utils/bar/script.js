fcc20e5d21c476d994e3ef48346aee995204d973
NoName-Coding <natur2.stadion@icloud.com>
2026-04-08 22:39:27 +0200

    Update script.js

--- docs/host/0/dsdmt/utils/bar/script.js
+++ docs/host/0/dsdmt/utils/bar/script.js
@@ -1,1 +1,20 @@
-
+const inhalte = [
+  ["up", "up.html"],
+  ["down", "down.html"]
+];
+inhalte.forEach(([id, datei]) => {
+  const element = document.getElementById(id);
+  if (element) {
+    fetch(datei)
+      .then(response => {
+        if (!response.ok) throw new Error(`Fehler beim Laden von ${datei}`);
+        return response.text();
+      })
+      .then(html => {
+        element.innerHTML = html;
+      })
+      .catch(error => console.error(error));
+  } else {
+    console.warn(`Kein Element mit der ID '${id}' gefunden.`);
+  }
+});
